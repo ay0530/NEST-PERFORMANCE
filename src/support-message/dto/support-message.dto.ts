231ef592709cf861757d7bf830fd03d520547a1b
ay0530 <edkdud530@gmail.com>
2023-12-28 16:59:10 +0900

    🎉 begin : 로그인 및 기본 기능들 세팅

--- src/support-message/dto/support-message.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator'; // 유효성 검사
-
-export class SupportMessageDto {
-  @IsString()
-  @IsNotEmpty({ message: '응원 메시지를 입력해주세요.' })
-  message: string;
-}
