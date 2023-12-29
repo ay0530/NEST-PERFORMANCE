01a1e32be2e2d8a746982618b660c6d9e95c10c1
ay0530 <edkdud530@gmail.com>
2023-12-29 17:04:35 +0900

    🩹 fix: USER 테이블 명 수정
    테이블명 : users -> USER

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { SupportMessage } from 'src/support-message/entities/support-message.entity'; // support-message 엔티티 사용
+// import { SupportMessage } from 'src/support-message/entities/support-message.entity'; // support-message 엔티티 사용
 import {
   Column,
   Entity,
@@ -11,7 +11,7 @@ import { Role } from '../types/userRole.type'; // userRole 타입(enum) 사용
 
 @Index('email', ['email'], { unique: true }) // index를 설정함으로써 검색 효율이 향상됨
 @Entity({
-  name: 'users', // 테이블 명
+  name: 'USER', // 테이블 명
 })
 export class User {
   @PrimaryGeneratedColumn() // 기본키 설정
@@ -28,6 +28,6 @@ export class User {
 
   // 일대다 관계 설정
   // (supportMessage) => supportMessage.user : supportMessage 엔티티 내의 'user' 필드를 참고
-  @OneToMany(() => SupportMessage, (supportMessage) => supportMessage.user)
-  supportMessages: SupportMessage[];
+  // @OneToMany(() => SupportMessage, (supportMessage) => supportMessage.user)
+  // supportMessages: SupportMessage[];
 }
